09688a43974f0b4efb4a68266938342c31acba4a
Reggie Jones <regjones16@outlook.com>
2024-08-16 09:52:42 -0500

    Initial Commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "reggie-talent-academy-686520628199-tfstates"
-    key            = "projects/project-vpc/terraform.tfstates"
+    key            = "projects/project-vpc/terraform2.tfstates"
     dynamodb_table = "terraform-lock"
   }
 }
