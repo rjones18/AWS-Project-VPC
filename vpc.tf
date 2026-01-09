8231039ebd647df9954fd817cdab56b4a551a3e3
Reginald Jones <regjones16@outlook.com>
2026-01-09 10:49:14 -0600

    Update vpc code to use vpc module I created

--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,24 @@
 module "vpc" {
   source = "git::https://github.com/rjones18/AWS-VPC-MODULE.git?ref=v1.0.0"
 
-  # VPC settings
-  vpc_name    = "project-vpc"
-  vpc_cidr    = "10.0.0.0/16"
+  vpc_cidr = "10.0.0.0/16"
 
-  # Subnets
-  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
-  availability_zones   = ["us-west-2a", "us-west-2b"]
+  public_subnet_cidrs = {
+    "us-east-1a" = "10.0.1.0/24"
+    "us-east-1b" = "10.0.2.0/24"
+  }
 
-  # Gateways
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_internet_gateway = true
+  private_subnet_cidrs = {
+    "us-east-1a" = "10.0.101.0/24"
+    "us-east-1b" = "10.0.102.0/24"
+  }
+
+  enable_nat_gateway = true
 
   tags = {
-    Environment = "dev"
-    Project     = "my-project"
+    Project = "platform"
+    Env     = "dev"
+    Owner   = "reggie"
+    Name = "project-vpc"
   }
 }
